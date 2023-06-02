c6470503f3c71f96d91dc53df2b51669ec1a6bf9
Georg Oeltzschner <goeltzs1@jh.edu>
2023-06-02 10:56:38 -0400

    Update niimrs.m
    Added plotSpec method

--- niimrs.m
+++ niimrs.m
@@ -1,6 +1,7 @@
 classdef niimrs
     % Standardized Processing Library Class
-    %   Detailed explanation goes here
+    %   Victor Han
+    %   Georg Oeltzschner
 
     properties
         hdr
@@ -19,10 +20,40 @@ classdef niimrs
             obj.img = temp.img;
         end
 
-        function plotAxis = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
+        function plotAxis = plotSpec(obj)
+            % PLOTSPEC
             %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+
+            % Get FID
+            fid = squeeze(obj.img);
+
+            % Get spectral width
+            sw = 1/obj.hdr.pixdim(5);
+
+            % Decode the JSON header extension string
+            header_extension = jsondecode(obj.ext.edata_decoded);
+
+            % Extract F0 and number of samples
+            f0 = header_extension.SpectrometerFrequency;
+            npts = obj.hdr.dim(5);
+
+            % Create frequency axis
+            f = [(-sw/2)+(sw/(2*npts)):sw/(npts):(sw/2)-(sw/(2*npts))];
+            % Convert to ppm
+            ppm = -f/f0;
+            ppm = ppm + 4.68;
+
+            % Calculate and plot the frequency domain spectrum
+            spec = fftshift(fft(fid));
+            plotAxis = plot(ppm, real(spec));
+            hold on;
+            plot(ppm, imag(spec));
+            set(gca, 'xdir', 'reverse', 'xlim', [0 5]);
+            xlabel('Chemical shift (ppm)');
+            legend('real', 'imag');
+            hold off;
+
+            
         end
     end
 end
